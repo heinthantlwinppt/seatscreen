bfbdfa9f5cdb5c29f647d2fc3c5164b07c3b61a6
heinthantlwinppt <heinthantlwin.ppt@gmail.com>
2018-08-17 09:48:22 +0630

    bus detail

---
+++ app/src/main/java/com/test/hein/seatscreen/busdetailactivity.java
@@ -0,0 +1,14 @@
+package com.test.hein.seatscreen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class busdetailactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_details_screen);
+    }
+}
